45a69f5ab0822a0f49ae9bd7c7dc5c40288e1bdd
Ihar Skavysh <igogoblin@gmail.com>
2024-08-25 15:10:39 +0300

    feat: add guess

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -8,6 +8,10 @@ class GuessingGame {
     this.minValue = min;
     this.maxValue = max;
   }
+
+  guess() {
+    return Math.round((this.maxValue - this.minValue) / 2);
+  }
 }
 
 module.exports = GuessingGame;
